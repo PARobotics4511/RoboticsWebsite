4e8a224aa4d903bf6c93c7fdb823d28b4a015318
unknown <Student@STU-ENG13.Provnet.local>
2016-03-02 12:02:33 -0600

    Fixes header and map center on resize

--- map.js
+++ map.js
@@ -1,25 +1,31 @@
-var map;
-var myLatLong = {lat:45.045179, lng:-93.471339};
-var marker;
+  function initialize() {
+        var mapOptions = {
+          zoom: 18,
+          center: new google.maps.LatLng(45.045179, -93.471339),
+          mapTypeId: google.maps.MapTypeId.SATELLITE
+        };
+        map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+        new google.maps.Marker({map:map,position:map.getCenter()})
+        
+        
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: myLatLong,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
+  google.maps.event.addListener(map, 'center_changed', function() {
+    
+    //a value to determine whether the map has been resized
+    var size=[this.getDiv().offsetWidth,this.getDiv().offsetHeight].join('x');
+    
+    //when the center has changed, but not the size of the map
+    if(!this.get('size') || size===this.get('size')){
+       console.log('ccc');
+       this.setValues({size:size,_center:this.getCenter()});         
+    }
+    //when the map has been resized
+    else{
+      google.maps.event.addListenerOnce(this,'idle',function(){console.log('rrr');
+      this.setValues({size:size,center:this.get('_center')});});      
+    }
   });
-
-  marker = new google.maps.Marker({
-    map: map,
-    animation: google.maps.Animation.BOUNCE,
-    position: myLatLong
-  });
-  marker.addListener('click', toggleBounce);
-}
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
+  //trigger the resize-event to initialize the size and _center-values
+  google.maps.event.trigger(map,'center_changed',{});
 }
